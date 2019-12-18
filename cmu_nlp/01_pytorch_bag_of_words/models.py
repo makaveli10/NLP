66ffe9c64e1793730f3897c66ed4fadd52f04fe1
makaveli10 <suryanvineet47@gmail.com>
2019-12-18 19:54:52 +0530

    created pytorch Deep continuous BoW

--- cmu_nlp/01_pytorch_bag_of_words/models.py
+++ cmu_nlp/01_pytorch_bag_of_words/models.py
@@ -18,6 +18,7 @@ class BoW(nn.Module):
         out = out.view(1, -1)   # size(out) = 1 x N
         return out
     
+
 class CBoW(nn.Module):
     def __init__(self, nwords, ntags, emb_size):
         super(CBoW, self).__init__()
@@ -32,3 +33,34 @@ class CBoW(nn.Module):
         out = emb_sum.view(1, -1) # size(emb_sum) = 1 x emb_size
         out = self.linear(out)
         return out
+    
+    
+
+class DeepCBow(nn.Module):
+    def __init__(self, nwords, ntags, emb_size, hid_size, n_layerss):
+        super(DeepCBoW, self).__init__()
+        self.nlayers = n_layers
+        self.embedding = nn.Embedding(nwords, emb_size)
+        nn.init.xavier_uniform_(self.embedding.weight)
+        
+        # create n_layers no of layers
+        self.hidden_layers = nn.ModuleList([
+                        nn.Linear(emb_size if i == 0 else hid_size, hid_size) \
+                        for i in range(n_layers)])
+        # initialize hidden layers
+        for i in range(n_layers):
+            nn.init.xavier_uniform_(self.hidden_layers[i].weight)
+           
+        # create output_layer
+        self.output = nn.Linear(hid_size, ntags)
+        nn.init.xavier_uniform_(self.output.weight)
+    
+    def forward(self, nwords):
+        emb_out = self.embedding(nwords)
+        emb_sum = torch.sum(emb_out, dim=0)     # size(emb_sum) = emb_size
+        h = emb_sum.view(1, -1)     # size(h) = 1 x emb_size
+        for i in range(self.nlayers):
+            h = torch.tanh(self.linear[i](h))
+        out = self.output_layer(h)
+        return out
+        
